55a929bed7bf7dbbf699edaeddf5504373cfc58d
Mustafo <madrahimov.mustafo@mail.ru>
2026-03-05 19:30:39 +0500

    Fix bugs

--- booking_project/settings.py
+++ booking_project/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config('SECRET_KEY', default='django-insecure-test-key-change-in-production')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEBUG', default=False, cast=bool)
+DEBUG = config('DEBUG', default=True, cast=bool)
 
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='localhost,127.0.0.1', cast=lambda v: [s.strip() for s in v.split(',')])
+ALLOWED_HOSTS = ['*']  # For development; restrict in production
 
 
 # Application definition
